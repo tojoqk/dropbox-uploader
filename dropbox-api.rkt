0a3fea664948c2d44cb127dc213d09bfacd46962
Masaya Tojo <masaya@tojo.tokyo>
2018-08-20 08:05:05 +0900

    Add list-folder

--- dropbox-api.rkt
+++ dropbox-api.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require (for-syntax racket/base racket/list syntax/parse syntax/flatten-begin))
 (require racket/contract net/http-client json)
+(require (only-in srfi/1 append-reverse))
 (define (access-token) (getenv "DROPBOX_ACCESS_TOKEN"))
 
 (define/contract (json-api host uri json #:headers [headers '()])
@@ -30,3 +31,31 @@
 (define (list_folder json)
   (json-api api.dropboxapi.com "/2/files/list_folder" json))
 
+(define (list_folder/continue json)
+  (json-api api.dropboxapi.com "/2/files/list_folder/continue" json))
+
+(define (list-folder dirs)
+  (define (continue cursor names)
+    (sleep 0.1)
+    (let-values ([(status headers containts) (list_folder/continue (hash 'cursor cursor))])
+      (match (and (ok? status) (read-json containts))
+        [(hash-table
+          ('has_more has-more?)
+          ('cursor cursor)
+          ('entries (list (hash-table ('name new-names)) ...)))
+         (let ([names (append-reverse new-names names)])
+           (if has-more?
+               (continue cursor names)
+               (reverse names)))]
+        [else #f])))
+  (define path (if (null? dirs) "" (string-append "/" (string-join dirs "/"))))
+  (let-values ([(status headers containts) (list_folder (hash 'path path))])
+    (match (and (ok? status) (read-json containts))
+      [(hash-table
+        ('has_more has-more?)
+        ('cursor cursor)
+        ('entries (list (hash-table ('name names)) ...)))
+       (if has-more?
+           (continue cursor (reverse names))
+           names)]
+      [else #f])))
