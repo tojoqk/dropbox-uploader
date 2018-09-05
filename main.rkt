89c927d71bba331738b355c43f6e9533ca2ac229
Masaya Tojo <masaya@tojo.tokyo>
2018-09-05 23:03:42 +0900

    Drop dropbox- prefix of call-with... procedures

--- main.rkt
+++ main.rkt
@@ -1,3 +1,5 @@
 #lang racket
 (require "dropbox.rkt")
-(provide [prefix-out dropbox- (all-from-out "dropbox.rkt")])
+(provide (rename-out (prefix-out dropbox- (all-from-out "dropbox.rkt"))
+                     [dropbox-call-with-input-from-dropbox call-with-input-from-dropbox]
+                     [dropbox-call-with-output-to-dropbox call-with-output-to-dropbox]))
